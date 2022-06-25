044c8e1aeae266e018a6150654c5d5b8d1d3d994
Junsu Min <sunsuking@gmail.com>
2022-06-25 19:41:56 +0900

    Temi Camera Permission

--- app/src/main/java/com/example/gardeners/MainActivity.java
+++ app/src/main/java/com/example/gardeners/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.gardeners;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -28,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity);
+
+        if (!hasPermissions(PERMISSIONS)) {
+            //퍼미션 허가 안되어있다면 사용자에게 요청
+            requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+        }
 
         homeFragment = new HomeFragment();
         plantFragment = new PlantFragment();
@@ -64,5 +76,57 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    static final int PERMISSIONS_REQUEST_CODE = 1000;
+    String[] PERMISSIONS  = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private boolean hasPermissions(String[] permissions) {
+        int result;
+
+        for (String perms : permissions){
+            result = ContextCompat.checkSelfPermission(this, perms);
+            if (result == PackageManager.PERMISSION_DENIED){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+                boolean cameraPermissionAccepted = grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED;
+                boolean diskPermissionAccepted = grantResults[1]
+                        == PackageManager.PERMISSION_GRANTED;
+
+                if (!cameraPermissionAccepted || !diskPermissionAccepted)
+                    showDialogForPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
+            }
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void showDialogForPermission(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this);
+        builder.setTitle("알림");
+        builder.setMessage(msg);
+        builder.setCancelable(false);
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id){
+                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                finish();
+            }
+        });
+        builder.create().show();
     }
 }
